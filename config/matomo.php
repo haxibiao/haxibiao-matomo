15989f6d238304127723ea8aaaa5b2f526839a9b
ivan <ivan@haxibiao.com>
2020-06-14 11:44:04 +0800

    默认压力小的时候maotmo3个统计来源可以在一起

--- config/matomo.php
+++ config/matomo.php
@@ -8,11 +8,11 @@ return [
     'matomo_id'  => env('MATOMO_ID'), //后端事件统计的site_id
     'matomo_url' => env('MATOMO_URL'), //后端事件统计查看的matomo url
 
-    'app_id'     => env('MATOMO_APP_ID'), //APP事件统计的site_id 前端可以直接上报了,基本不借道后端了
-    'app_url'    => env('MATOMO_APP_URL'), //APP事件统计查看的matomo url
+    'app_id'     => env('MATOMO_APP_ID', env('MATOMO_ID')), //APP事件统计的site_id 前端可以直接上报了,基本不借道后端了
+    'app_url'    => env('MATOMO_APP_URL', env('MATOMO_URL')), //APP事件统计查看的matomo url
 
-    'web_id'     => env('MATOMO_WEB_ID'), //Web事件统计的site_id
-    'web_url'    => env('MATOMO_WEB_URL'), //Web事件统计查看的matomo url
+    'web_id'     => env('MATOMO_WEB_ID', env('MATOMO_ID')), //Web事件统计的site_id
+    'web_url'    => env('MATOMO_WEB_URL', env('MATOMO_URL')), //Web事件统计查看的matomo url
 ];
 
 // 答赚示范(区分了后端，前端 和Web),也可以混合一起配置一样即可
