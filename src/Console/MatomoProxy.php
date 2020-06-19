d4c0506e66e8a6eb2c917d4581dcb32a333d9ec9
ivan <ivan@haxibiao.com>
2020-06-19 12:23:26 +0800

    修复机型统计出错

--- src/Console/MatomoProxy.php
+++ src/Console/MatomoProxy.php
@@ -169,7 +169,7 @@ class MatomoProxy extends Command
                     $tracker->setCustomVariable(3, '版本', $event->dimension3, 'visit');
                     $tracker->setCustomVariable(4, '用户', $event->dimension4, 'visit');
                     $tracker->setCustomVariable(5, "服务器", $event->server, "visit");
-                    $tracker->setCustomVariable(2, '机型', $event->dimension5, 'visit');
+                    $tracker->setCustomVariable(6, '机型', $event->dimension5, 'visit');
 
                     // $url = $tracker->getUrlTrackEvent($event->category, $event->action, $event->name, $event->value);
                     //send
